86b583a1d07b3773e939f954abc7be674521437d
Fusilladin <fusilladin@protonmail.com>
2022-06-29 18:45:18 -0500

    Updated video notes

--- APIBasics.py
+++ APIBasics.py
@@ -1,9 +1,29 @@
+### API Basics
+
+# Rest API is the standard across the entire internet
+# URI = Universal Resource Identifier
+# HTTP = Hyper Text Transfer Protocol
+# HTML = Hyper Text Markup Language
+    # <a> uses Anchor Tags <a>
+
+# can also do a URI post instead of URL get
+
+# Constraints to be considered Restful
+    # 1. Client-Server Architecture
+    # 2. Statelessness
+    # 3. Layered System
+    # 4. Cacheability
+    # 5. Uniform Design
+    # 6. Code on Demand
+
+# go to developers page and utilize API to generate anything
 
 
 
-modified
 
-modified
+
+
+
 
 
 
